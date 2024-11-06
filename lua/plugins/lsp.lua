f460a74c6fc500c696701a075d4939a7f1fa314f
Luqmanul Hakim <luqmanulhakim1720@gmail.com>
2024-11-06 20:28:52 +0800

    add diagnostic float

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -124,6 +124,11 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
 
+          -- Opens a popup that displays diagnostics under the cursor
+          vim.keymap.set('n', '<leader>ch', function()
+            vim.diagnostic.open_float { scope = 'cursor' }
+          end, { buffer = event.buf, desc = 'LSP: Diagnostics Float' })
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
