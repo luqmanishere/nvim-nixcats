bfcff2bb9beddf441c7fffb70489ce8c3c3e9794
Luqmanul Hakim <luqmanulhakim1720@gmail.com>
2025-02-09 18:56:38 +0800

    add more options

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -32,6 +32,12 @@ return {
       nerd_font_variant = 'mono',
     },
 
+    completion = {
+      menu = { border = 'single' },
+      documentation = { window = { border = 'single' } },
+      accept = { auto_brackets = { enabled = true } },
+    },
+
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
